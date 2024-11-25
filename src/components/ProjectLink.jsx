d9294e6596311071debdcc41e9da23351aec7193
Felix Nagy <fnagy@st.noorderpoort.nl>
2024-11-25 11:35:49 +0100

    projects real data

--- src/components/ProjectLink.jsx
+++ src/components/ProjectLink.jsx
@@ -1,29 +1,22 @@
 import * as Icon from "react-feather";
-import { Tooltip } from "react-tooltip";
 import Button from "./Button";
 
 const darkMode = localStorage.getItem("theme") === "dark";
 
-const ProjectLink = ({ title, imageUrl, githubUrl, liveUrl }) => {
+const ProjectLink = ({ title, imageUrl, githubUrl }) => {
   return (
-    <div className="project-link dark:bg-primary group relative flex flex-col overflow-hidden rounded-lg border border-gray-300 bg-white shadow-md dark:border-gray-700">
+    <div className="project-link dark:bg-primary relative flex flex-col overflow-hidden border-2 border-pri bg-sec dark:border-gray-700">
       {/* Project Image */}
       <img
         src={imageUrl}
         alt={title}
-        className="h-48 w-full object-cover transition-transform duration-300 group-hover:scale-105"
+        className="h-64 w-full select-none border-b-2 border-pri object-cover transition-transform duration-300"
+        draggable="false"
       />
 
-      {/* Overlay Title (can use pointer-events-none)*/}
-
-      {/* ! TODO: put img in public folder because its not visible on live site.... and users should be allowed to view it riht? with right click view. and then it'll go to /slate.webp or/orange.webp */}
-
-      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-        <h2 className="text-lg font-semibold text-white">{title}</h2>
-      </div>
-
       {/* Button Section */}
-      <div className="flex justify-between gap-2 p-4">
+      <div className="flex items-center justify-between gap-2 p-2">
+        <h3 className="select-none text-lg font-medium">{title}</h3>
         <Button
           data-tooltip-id="github"
           data-tooltip-content="View on GitHub"
@@ -34,15 +27,6 @@ const ProjectLink = ({ title, imageUrl, githubUrl, liveUrl }) => {
         >
           <Icon.GitHub />
         </Button>
-        <Button
-          href={liveUrl}
-          target="_blank"
-          className="flex items-center gap-1 rounded-md border border-blue-500 bg-blue-500 px-4 py-2 text-sm text-white hover:bg-blue-600"
-        >
-          <Icon.ExternalLink />
-          Visit
-        </Button>
-        <Tooltip id="github" />
       </div>
     </div>
   );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,9 @@
-import ProjectLink from "../components/ProjectLink";
+import ProjectLink from "../components/Projectlink";
 
 export default function Projects() {
   return (
     <div>
-      <div className="dark:text-sec flex flex-col gap-4 lg:max-w-[50%]">
+      <div className="flex flex-col gap-4 dark:text-sec lg:max-w-[50%]">
         <h2>My work</h2>
         <p>
           Here are a few projects I’ve worked on recently. They showcase my
@@ -12,28 +12,19 @@ export default function Projects() {
       </div>
       <div className="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
         <ProjectLink
-          title="Project 1"
-          imageUrl="/src/img/slate.webp"
-          githubUrl="https://github.com/fexgroot/project-1"
-          liveUrl="https://project-1-live-site.com"
+          title="Grumpy Tasks"
+          imageUrl="public/img/grumpy-tasks.png"
+          githubUrl="https://github.com/fexgroot/grumpy-tasks"
         />
         <ProjectLink
-          title="Project 2"
-          imageUrl="/src/img/orange.webp"
-          githubUrl="https://github.com/fexgroot/project-2"
-          liveUrl="https://project-2-live-site.com"
+          title="Tasker"
+          imageUrl="public/img/tasker.png"
+          githubUrl="https://github.com/fexgroot/tasker"
         />
         <ProjectLink
           title="Project 3"
-          imageUrl="/src/img/slate.webp"
+          imageUrl="public/img/slate.webp"
           githubUrl="https://github.com/fexgroot/project-3"
-          liveUrl="https://project-3-live-site.com"
-        />
-        <ProjectLink
-          title="Project 4"
-          imageUrl="/src/img/orange.webp"
-          githubUrl="https://github.com/fexgroot/project-4"
-          liveUrl="https://project-4-live-site.com"
         />
       </div>
     </div>
